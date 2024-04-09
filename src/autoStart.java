1a0eecbed9a25e89e9fe5ad85e7c2fd1c1c6f787
Cenzell <ethanhartshorn488@outlook.com>
2024-04-08 21:51:51 -0500

    first commit

--- src/autoStart.java
+++ src/autoStart.java
@@ -1,7 +1,40 @@
-class Test
+import java.io.File;
+
+
+class klipperService
 {
     public static void main(String []args)
     {
-        System.out.println("My First Java Program.");
+        System.out.println("AutoStart Running...");
+        
+        boolean printerConnected = false;
+        boolean klipperOnline = false;
+        
+        while (true) {
+        
+            File printerPath = new File("/dev/serial/by-id/usb-Klipper_stm32h723xx_170035001651313238353730-if00");
+
+            try {
+                Thread.sleep(15000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        
+            if(printerPath.exists()){
+                printerConnected = true;
+            }
+
+            if(printerConnected && klipperOnline){
+                connectKlipper();
+            }
+        }
     }
+
+    public static void connectKlipper(){
+        //Restart Klipper Code
+    }
+
+    public static void getKlipperStatus(){
+        //Code to get the status of Klipper
+    } 
 };
